6e2bd3df66e3518f48ed813209d22eb934bca21d
Jitender Kumar <jitenderkumar@Jitenders-MacBook-Air.local>
2025-10-29 14:23:53 +0530

    ok

--- utils/sendNotification.js
+++ utils/sendNotification.js
@@ -66,7 +66,7 @@ const initializeFirebase = () => {
       type: process.env.FIREBASE_TYPE || "service_account",
       project_id: process.env.FIREBASE_PROJECT_ID,
       private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       client_email: process.env.FIREBASE_CLIENT_EMAIL || "firebase-adminsdk-fbsvc@olyox-6215a.iam.gserviceaccount.com",
       client_id: process.env.FIREBASE_CLIENT_ID,
       auth_uri: process.env.FIREBASE_AUTH_URI,
